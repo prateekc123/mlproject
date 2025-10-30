205ed6ae1b479b64230c6bebe6b63bff17bd91a9
Prateek Chaturvedi <prateekc123@gmail.com>
2025-10-31 01:58:33 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 from src.exception import CustomException
 import dill
+from sklearn.metrics import r2_score
 
 def save_object(file_path,obj):
     try:
@@ -16,3 +17,17 @@ def save_object(file_path,obj):
     
     except Exception as e:
         raise CustomException(e,sys)
+
+from sklearn.metrics import r2_score
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    report = {}
+    for name, model in models.items():
+        try:
+            model.fit(X_train, y_train)
+            y_pred = model.predict(X_test)
+            score = r2_score(y_test, y_pred)
+            report[name] = score
+        except Exception as e:
+            report[name] = None  # or skip this model
+    return report
